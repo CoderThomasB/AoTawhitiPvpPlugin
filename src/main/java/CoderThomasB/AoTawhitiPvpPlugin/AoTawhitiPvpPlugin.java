010a1ad573ca8ed8ea59645b89000f7c40c2a78a
CoderThomasB <tw.booker@outlook.com>
2021-05-12 19:49:20 +1200

    Initial commit

---
+++ src/main/java/CoderThomasB/AoTawhitiPvpPlugin/AoTawhitiPvpPlugin.java
@@ -0,0 +1,11 @@
+package CoderThomasB.AoTawhitiPvpPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AoTawhitiPvpPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello");
+    }
+}
